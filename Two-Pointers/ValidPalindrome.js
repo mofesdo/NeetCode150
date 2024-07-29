12afdea0df0ce723794b5073e1528d10d8a69023
Modesto Amador <mofesdo.dev@gmail.com>
2024-07-28 18:37:20 -0700

    cleaned up code

--- Two-Pointers/ValidPalindrome.js
+++ Two-Pointers/ValidPalindrome.js
@@ -30,12 +30,9 @@ class Solution {
     isPalindrome(s) {
         //First make the string all lowercase and only letters.
         s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-        console.log(s);
         //split string in half and check if front and back match
         for(let i = 0; i < s.length/2; i++){
             if(s[i] != s[s.length-1-i]){
-                console.log(s[i]);
-                console.log(s[s.length-1-i]);
                 return false;
             }
         }
